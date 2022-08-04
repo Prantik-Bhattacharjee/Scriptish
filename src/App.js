f4241078f94eb8e41088d8465e0903e40a4786ab
Prantik-Bhattacharjee <prantikbhatt2001@gmail.com>
2022-08-04 23:59:57 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Registration from './components/Registration/Registration'
+
+function App() {
+  return (
+    <div className="App">
+     <Registration />
+    </div>
+  );
+}
+
+export default App;
